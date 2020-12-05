47b29908c712bbb10ae00485a4aacd32c70cb524
43081j <43081j@users.noreply.github.com>
2020-12-05 14:51:19 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+[![npm](https://img.shields.io/npm/v/hanbi-chai)](https://www.npmjs.com/package/hanbi-chai)
+
+# hanbi-chai
+
+hanbi-chai is a small set of extensions to
+[chai](https://github.com/chaijs/chai) for the
+[hanbi](https://github.com/43081j/hanbi) testing library.
+
+# Install
+
+```
+$ npm i -D hanbi-chai
+```
+
+# Usage
+
+To use these extensions, simply call `chai.use` like so:
+
+```ts
+import hanbiChai from 'hanbi-chai';
+import * as chai from 'chai';
+
+chai.use(hanbiChai);
+```
+
+# API
+
+TBD
---
+++ src/main.ts
@@ -0,0 +1,70 @@
+/// <reference types="chai" />
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Chai {
+    interface Assertion {
+      called: Chai.Assertion;
+      callCount(count: number): Chai.Assertion;
+      calledWith(...args: unknown[]): Chai.Assertion;
+      returned(val: unknown): Chai.Assertion;
+    }
+  }
+}
+
+const plugin: Chai.ChaiPlugin = (chai) => {
+  const Assertion = chai.Assertion;
+  Assertion.addProperty('called', function called(this: Chai.AssertionStatic) {
+    this.assert(
+      this._obj.called === true,
+      'expected #{this} to have been called',
+      'expected #{this} not to have been called',
+      true,
+      this._obj.called
+    );
+  });
+
+  Assertion.addMethod(
+    'callCount',
+    function callCount(this: Chai.AssertionStatic, count: number): void {
+      this.assert(
+        this._obj.callCount === count,
+        'expected #{this} to have been called #{exp} times but ' +
+          'was called #{act} times',
+        'expected #{this} not to have been called #{exp} times',
+        count,
+        this._obj.callCount
+      );
+    }
+  );
+
+  Assertion.addMethod(
+    'calledWith',
+    function calledWith(this: Chai.AssertionStatic, ...args: unknown[]): void {
+      const result = this._obj.calledWith(...args);
+      this.assert(
+        result,
+        'expected #{this} to have been called with #{exp}',
+        'expected #{this} not to have been called with #{exp}',
+        true,
+        result
+      );
+    }
+  );
+
+  Assertion.addMethod(
+    'returned',
+    function returned(this: Chai.AssertionStatic, val: unknown): void {
+      const result = this._obj.returned(val);
+      this.assert(
+        result,
+        'expected #{this} to have returned #{exp}',
+        'expected #{this} not to have returned #{exp}',
+        true,
+        result
+      );
+    }
+  );
+};
+
+export default plugin;
